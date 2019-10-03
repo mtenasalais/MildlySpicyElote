873d0d4240b0871e61ff4e0ca62f77765215aa79
Spencer Duball <spencerduball@gmail.com>
2019-10-03 09:31:49 -0500

    saving here

--- app.js
+++ app.js
@@ -115,7 +115,6 @@ setInterval(() => {
           if (lcd_on) writeToLcd(my_lcd, CODE.NO_ERRORS, temp);
         }
       } else {
-        lcd_power.write(0);
         postRequest(CODE.SENSOR_ERROR, null);
         if (lcd_on) writeToLcd(my_lcd, CODE.SENSOR_ERROR, null);
         console.log("Sensor Error: Error reading sensor.");
