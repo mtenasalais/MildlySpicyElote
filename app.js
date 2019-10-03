5a1e396cbf864488632c0676a3635e5443eb0753
Spencer Duball <spencer-duball@uiowa.edu>
2019-10-03 09:21:32 -0500

    hot fix

--- app.js
+++ app.js
@@ -100,8 +100,8 @@ const turnLcdOff = (lcd_power, state) => {
 const lcd_power = new Gpio(26, "out");
 
 // create my_lcd
-let state = { lcd_on: false };
-let my_lcd = turnLcdOn(lcd_power);
+const state = { lcd_on: false };
+let my_lcd = turnLcdOn(lcd_power, state);
 
 // main loop
 const DEFAULT_TEMP = 0;
